834c80106aa1bf666167c0bbb25d8aaa82f12b24
Abdessamad7687 <abdessamadahmali@gmail.com>
2023-03-03 13:24:20 +0100

    Tailwins~CSS

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,11 +3,11 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Card = ({ userId, title, body }) => {
     return (
-        <div className="card bg-light mb-3 w-25 m-2 mx-auto">
-            <p className="card-header">UserId: {userId}</p>
-            <div className="card-body">
-                <h6 className="card-title"><strong>Title</strong>: {title}</h6>
-                <p className="card-text">{body}</p>
+        <div className="relative flex flex-col min-w-0 rounded break-words border bg-white border-1 border-gray-300 bg-gray-100 mb-3 w-1/4 m-2 mx-auto">
+            <p className="py-3 px-6 mb-0 bg-gray-200 border-b-1 border-gray-300 text-gray-900">UserId: {userId}</p>
+            <div className="flex-auto p-6">
+                <h6 className="mb-3"><strong>Title</strong>: {title}</h6>
+                <p className="mb-0">{body}</p>
             </div>
         </div>
     )
